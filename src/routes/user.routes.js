5f5ce886776fd96a295597af5ffe44bdbd7ae376
Neeraj Bora <121128050+Neeraj-Bora@users.noreply.github.com>
2024-08-08 03:21:01 +0530

    fixing error

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { registerUser } from '../controlleers/user.controller';
+import { registerUser } from '../controlleers/user.controller.js';
 
 const router = Router();
 
