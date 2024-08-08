7c1175c4173c3912fb3c139e114876234edd7747
Neeraj Bora <121128050+Neeraj-Bora@users.noreply.github.com>
2024-08-09 00:24:04 +0530

    Database setup

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser())
 import userRouter from './routes/user.routes.js'
 
 //routes declaration
-app.use("/api/v1/user", userRouter)
+app.use("/api/v1/users", userRouter)
 
 //our url http://localhost:8000/api/v1/users/register
 
--- src/controlleers/user.controller.js
+++ src/controlleers/user.controller.js
@@ -1,7 +1,7 @@
-import {asyncHandler} from '../utils/asyncHandler.js'
+import {asyncHandler} from '../utils/asyncHandler.js';
 
 
-const registerUser = asyncHandler( async (req,res)=>{
+const registerUser = asyncHandler( async (req,res,next)=>{
     res.status(200).json({
         message:"ok"
     })
